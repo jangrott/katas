c10b5f6420d823d014f0151413964662082ced47
tjangrot <tomek.jangrot@gmail.com>
2015-04-29 21:03:18 +0200

    added project structure

---
+++ roman2arabic/impl-in-python/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Roman2Arabic',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Tomasz Jangrot',
+      author_email='tomek.jangrot@gmail.com',
+      url='https://github.com/jangrott/katas/tree/master/roman2arabic/impl-in-python',
+      packages=['roman2arabic'],
+     )
