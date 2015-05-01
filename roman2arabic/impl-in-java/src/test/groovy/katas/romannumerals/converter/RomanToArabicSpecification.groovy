65130f4a9be4066e42a78dcf0f3c635006cb1e29
tjangrot <tomek.jangrot@gmail.com>
2015-05-01 14:21:55 +0200

    added spock for testing (changed way of tests for converter)

---
+++ roman2arabic/impl-in-java/src/test/groovy/katas/romannumerals/converter/RomanToArabicSpecification.groovy
@@ -0,0 +1,40 @@
+import katas.romannumerals.converter.RomanToArabic
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import static katas.romannumerals.ArabicNumeral.arabicNumeralOf
+import static katas.romannumerals.RomanNumeral.romanNumeralOf
+
+@Unroll
+class RomanToArabicSpecification extends Specification {
+
+    def "The arabic conversion of #romanValue is #arabicValue"() {
+        expect:
+            romanNumeralOf(romanValue).to(new RomanToArabic()) == arabicNumeralOf(arabicValue)
+        where:
+            romanValue      ||  arabicValue
+            "I"             ||  1
+            "V"             ||  5
+            "X"             ||  10
+            "L"             ||  50
+            "C"             ||  100
+            "D"             ||  500
+            "M"             ||  1000
+            "IV"            ||  4
+            "VI"            ||  6
+            "VII"           ||  7
+            "VIII"          ||  8
+            "IX"            ||  9
+            "XXVII"         ||  27
+            "CCCXXV"        ||  325
+            "CDXXXII"       ||  432
+            "DCCCXLV"       ||  845
+            "MCMXLIV"       ||  1944
+            "MCMXLVII"      ||  1947
+            "MCMXLVIII"     ||  1948
+            "MM"            ||  2000
+            "MMDCCCXIX"     ||  2819
+            "MMDCCCXVIII"   ||  2818
+            "MMMDXLIX"      ||  3549
+    }
+}
--- roman2arabic/impl-in-java/src/test/java/katas/romannumerals/converter/RomanToArabicTest.java
+++ roman2arabic/impl-in-java/src/test/java/katas/romannumerals/converter/RomanToArabicTest.java
@@ -1,67 +1,13 @@
 package katas.romannumerals.converter;
 
-import katas.romannumerals.ArabicNumeral;
-import katas.romannumerals.RomanNumeral;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-
-import java.util.Arrays;
-import java.util.Collection;
 
 import static katas.romannumerals.ArabicNumeral.arabicNumeralOf;
-import static katas.romannumerals.RomanNumeral.romanNumeralOf;
-import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(Parameterized.class)
 public class RomanToArabicTest {
-    private RomanToArabic arabicNumeralConverter;
 
-    private RomanNumeral input;
-    private ArabicNumeral output;
-
-    @Parameterized.Parameters
-    public static Collection params() {
-        return Arrays.asList(new Object[][]{
-                {romanNumeralOf("I"), arabicNumeralOf(1)},
-                {romanNumeralOf("V"), arabicNumeralOf(5)},
-                {romanNumeralOf("X"), arabicNumeralOf(10)},
-                {romanNumeralOf("L"), arabicNumeralOf(50)},
-                {romanNumeralOf("C"), arabicNumeralOf(100)},
-                {romanNumeralOf("D"), arabicNumeralOf(500)},
-                {romanNumeralOf("M"), arabicNumeralOf(1000)},
-                {romanNumeralOf("IV"), arabicNumeralOf(4)},
-                {romanNumeralOf("VI"), arabicNumeralOf(6)},
-                {romanNumeralOf("VII"), arabicNumeralOf(7)},
-                {romanNumeralOf("VIII"), arabicNumeralOf(8)},
-                {romanNumeralOf("IX"), arabicNumeralOf(9)},
-                {romanNumeralOf("XXVII"), arabicNumeralOf(27)},
-                {romanNumeralOf("CCCXXV"), arabicNumeralOf(325)},
-                {romanNumeralOf("CDXXXII"), arabicNumeralOf(432)},
-                {romanNumeralOf("DCCCXLV"), arabicNumeralOf(845)},
-                {romanNumeralOf("MCMXLIV"), arabicNumeralOf(1944)},
-                {romanNumeralOf("MCMXLVII"), arabicNumeralOf(1947)},
-                {romanNumeralOf("MCMXLVIII"), arabicNumeralOf(1948)},
-                {romanNumeralOf("MM"), arabicNumeralOf(2000)},
-                {romanNumeralOf("MMDCCCXIX"), arabicNumeralOf(2819)},
-                {romanNumeralOf("MMDCCCXVIII"), arabicNumeralOf(2818)},
-                {romanNumeralOf("MMMDXLIX"), arabicNumeralOf(3549)}
-        });
-    }
-
-    @Before
-    public void setUp() {
-        arabicNumeralConverter = new RomanToArabic();
-    }
-
-    public RomanToArabicTest(RomanNumeral romanNumeral, ArabicNumeral arabicNumeral) {
-        input = romanNumeral;
-        output = arabicNumeral;
-    }
-
-    @Test
-    public void canConvert() {
-        assertThat(arabicNumeralConverter.convert(input)).isEqualTo(output);
+    @Test(expected = UnsupportedOperationException.class)
+    public void exceptionShouldBeThrownWhenDoBackwardCalled() {
+        new RomanToArabic().doBackward(arabicNumeralOf(1));
     }
 }
