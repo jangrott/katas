e09d4dae537d30848f5eaaf08ff24850a6b40072
tjangrot <tomek.jangrot@gmail.com>
2015-05-05 22:03:25 +0200

    removed duplicate test

--- roman2arabic/impl-in-java/src/test/java/katas/romannumerals/RomanNumeralTest.java
+++ roman2arabic/impl-in-java/src/test/java/katas/romannumerals/RomanNumeralTest.java
@@ -1,22 +1,14 @@
 package katas.romannumerals;
 
-import katas.romannumerals.converter.RomanToArabic;
 import org.junit.Test;
 
-import static katas.romannumerals.ArabicNumeral.arabicNumeralOf;
 import static katas.romannumerals.RomanNumeral.romanNumeralOf;
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class RomanNumeralTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void exceptionShouldBeThrownWhenNullPassed() {
         romanNumeralOf(null);
-    }
-
-    @Test
-    public void canBeConvertToArabic() {
-        assertThat(romanNumeralOf("VIII").to(new RomanToArabic())).isEqualTo(arabicNumeralOf(8));
     }
 
     @Test(expected = IllegalArgumentException.class)
