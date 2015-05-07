bee54dac70ae3968edd22f01841c81cd552d6d00
tjangrot <tomek.jangrot@gmail.com>
2015-05-07 09:01:57 +0200

    changed names for lambdas

--- roman2arabic/impl-in-java/src/main/java/katas/romannumerals/converter/RomanToArabic.java
+++ roman2arabic/impl-in-java/src/main/java/katas/romannumerals/converter/RomanToArabic.java
@@ -4,10 +4,7 @@ import com.google.common.collect.Lists;
 import katas.romannumerals.ArabicNumeral;
 import katas.romannumerals.RomanNumeral;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 
@@ -19,26 +16,27 @@ public class RomanToArabic implements Converter<RomanNumeral, ArabicNumeral> {
 
     @Override
     public ArabicNumeral convert(RomanNumeral roman) {
-        int arabicValue = Lists
+        Optional<Integer> arabicValue = Lists
                 .reverse(splitGroupsOfSameChars(roman))
                 .stream()
-                .map(valueOfGroup)
-                .reduce(0, accumulate);
+                .map(toArabicValueOfGroup)
+                .reduce(toArabicValue);
 
-        return arabicNumeralOf(arabicValue);
+        return arabicNumeralOf(arabicValue.get());
     }
 
     private List<String> splitGroupsOfSameChars(RomanNumeral roman) {
         return Arrays.asList(roman.split(PATTERN_FOR_GROUPING_REPEATED_CHARS));
     }
 
-    private Function<String, Integer> valueOfGroup = group -> BasicMapping.valueOf(singleCharOf(group)) * group.length();
+    private Function<String, Integer> toArabicValueOfGroup =
+            group -> BasicMapping.valueOf(singleCharOf(group)) * group.length();
 
     private char singleCharOf(String s) {
         return s.charAt(0);
     }
 
-    private BinaryOperator<Integer> accumulate = (acc, cur) -> acc <= cur ? acc + cur : acc - cur;
+    private BinaryOperator<Integer> toArabicValue = (acc, cur) -> acc <= cur ? acc + cur : acc - cur;
 
     static class BasicMapping {
         private static final Map<Character, Integer> numerals = new HashMap<>();
